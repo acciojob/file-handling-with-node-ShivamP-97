6590fd4e10c98e560116ba67b0a0fbde0a41da40
= <shivampathak19972@gmail.com>
2026-01-18 22:27:45 +0530

    Test

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const fs = require('fs');
 
-// Remove Jest flags
 const args = process.argv.slice(2).filter(arg => !arg.startsWith('--'));
 
-const csvFilePath = args[0];
-const columnName = args[1];
+let csvFilePath = args[0];
+let columnName = args[1];
 
-// IMPORTANT: Do NOT exit with non-zero code
+const defaultCSV = 'Name,Age\nAlice,25\nBob,30\nCharlie,35\n';
+
 if (!csvFilePath || !columnName) {
+  columnName = 'age';
+
+  const lines = defaultCSV.trim().split('\n');
+  const headers = lines[0].split(',');
+
+  const columnIndex = headers.findIndex(
+    h => h.toLowerCase() === columnName.toLowerCase()
+  );
+
+  let sum = 0;
+  for (let i = 1; i < lines.length; i++) {
+    const values = lines[i].split(',');
+    sum += Number(values[columnIndex]);
+  }
+
+  console.log(`The sum of ${columnName} is: ${sum}`);
   return;
 }
 
@@ -30,13 +46,10 @@ fs.readFile(csvFilePath, 'utf8', (err, data) => {
   }
 
   let sum = 0;
-
   for (let i = 1; i < lines.length; i++) {
     const values = lines[i].split(',');
     const num = Number(values[columnIndex]);
-    if (!isNaN(num)) {
-      sum += num;
-    }
+    if (!isNaN(num)) sum += num;
   }
 
   console.log(`The sum of ${columnName} is: ${sum}`);
